1557957473eb1a5385987062ea877bb402b8ca49
k458 <koreec499@gmail.com>
2026-07-16 13:00:20 +0900

    Add line of sight check service

---
+++ LineOfSight/ILineOfSightChecker.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+namespace LineOfSight;
+
+public interface ILineOfSightChecker
+{
+    bool IsLineOfSightClear(Vector2 from, Vector2 to);
+    bool IsLineOfSightClearBypassingObstacles(Vector2 from, Vector2 to);
+}
---
+++ LineOfSight/LineOfSightCheckService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace LineOfSight;
+
+public static class LineOfSightCheckService
+{
+    public static ILineOfSightChecker? LineOfSightChecker { get; set; }
+
+    public static bool IsLineOfSightClear(
+        Vector2 from,
+        Vector2 to,
+        bool bypassObstacles)
+    {
+        ILineOfSightChecker checker = LineOfSightChecker
+            ?? throw new InvalidOperationException("Line of sight checker is not assigned.");
+
+        return bypassObstacles
+            ? checker.IsLineOfSightClearBypassingObstacles(from, to)
+            : checker.IsLineOfSightClear(from, to);
+    }
+}
