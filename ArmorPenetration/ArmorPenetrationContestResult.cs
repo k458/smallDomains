7ecc479ba7b47869d772ff34501a1f5e51cb3b0d
k458 <koreec499@gmail.com>
2026-07-30 13:01:12 +0900

    Add armor penetration contest formula

---
+++ ArmorPenetration/ArmorPenetrationContestResult.cs
@@ -0,0 +1,15 @@
+﻿namespace ArmorPenetration;
+
+public readonly struct ArmorPenetrationContestResult
+{
+    public ArmorPenetrationContestResult(float armorStrength, float penetrationStrength, float armorContestMultiplier)
+    {
+        ArmorStrength = armorStrength;
+        PenetrationStrength = penetrationStrength;
+        ArmorContestMultiplier = armorContestMultiplier;
+    }
+
+    public float ArmorStrength { get; }
+    public float PenetrationStrength { get; }
+    public float ArmorContestMultiplier { get; }
+}
--- ArmorPenetration/ArmorPenetrationService.cs
+++ ArmorPenetration/ArmorPenetrationService.cs
@@ -3,6 +3,7 @@
 public class ArmorPenetrationService
 {
     public float ArmorScalingConstant { get; set; } = 0.01f;
+    public float ArmorContestExponent { get; set; } = 3f;
 
     public ArmorPenetrationResult Calculate(float armor, float armorPenetration)
     {
@@ -10,5 +11,14 @@ public class ArmorPenetrationService
         float damageMultiplier = 1f / (1f + effectiveArmor * ArmorScalingConstant);
 
         return new ArmorPenetrationResult(effectiveArmor, damageMultiplier);
+    }
+
+    public ArmorPenetrationContestResult CalculateArmorContest(float armor, float armorPenetration)
+    {
+        float armorStrength = MathF.Pow(1f + armor * ArmorScalingConstant, ArmorContestExponent);
+        float penetrationStrength = MathF.Pow(1f + armorPenetration * ArmorScalingConstant, ArmorContestExponent);
+        float armorContestMultiplier = armorStrength / (armorStrength + penetrationStrength);
+
+        return new ArmorPenetrationContestResult(armorStrength, penetrationStrength, armorContestMultiplier);
     }
 }
